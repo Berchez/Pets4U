9c3ea39ecbafe1a65b729b75fd5b0f7f5130a080
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2021-03-21 21:48:10 -0300

    confirmar senha

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
-var check = function() {
-  if (document.getElementById('senha_cad').value ==
-      document.getElementById('confirmaSenha').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching';
+const passC = document.querySelector("#confirmaSenha");
+const pass = document.querySelector("#senha_cad");
+
+passC.addEventListener("keyup", () => {
+  check();
+});
+
+pass.addEventListener("keyup", () => {
+  check();
+});
+
+let check = () => {
+  if (
+    document.getElementById("senha_cad").value ==
+    document.getElementById("confirmaSenha").value
+  ) {
+    document.getElementById("message").style.color = "green";
+    document.getElementById("message").innerHTML = "Senhas Iguais";
   } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching';
+    document.getElementById("message").style.color = "red";
+    document.getElementById("message").innerHTML = "As senhas diferem";
   }
-}
+};
