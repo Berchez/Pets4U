a6f7a5e5f197ceef171ea065b630f139dc1dbde3
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2021-03-22 18:31:53 -0300

    update

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,7 @@ const { route } = require("./auth");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('login');
+    res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
