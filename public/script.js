891720e1e49a672f5855370fdfcb3d38a382376c
Walter Berchez <50505615+Berchez@users.noreply.github.com>
2021-03-24 12:03:49 -0300

    fix carousel resposible

--- public/script.js
+++ public/script.js
@@ -3,22 +3,27 @@ const pass = document.querySelector("#senha_cad");
 const mesage = document.querySelector("#message");
 
 passC.addEventListener("keyup", () => {
-  check();
+    check();
 });
 
 pass.addEventListener("keyup", () => {
-  check();
+    check();
 });
 
 let check = () => {
-  if (pass.value == passC.value) {
-    mesage.innerHTML = "Senhas Iguais";
-    mesage.style.backgroundColor = "rgba(11, 156, 49, 0.6)";
-    mesage.style.width = "12ch";
-  } else {
-    mesage.innerHTML = "As senhas diferem";
-    mesage.style.width = "15ch";
-    mesage.style.backgroundColor = "rgba(250, 0, 0, 0.6)";
-  }
-  mesage.classList.remove("hide");
+    if (pass.value == passC.value) {
+        mesage.innerHTML = "Senhas Iguais";
+        mesage.style.backgroundColor = "rgba(11, 156, 49, 0.6)";
+        mesage.style.width = "12ch";
+    } else {
+        mesage.innerHTML = "As senhas diferem";
+        mesage.style.width = "15ch";
+        mesage.style.backgroundColor = "rgba(250, 0, 0, 0.6)";
+    }
+    mesage.classList.remove("hide");
 };
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("nav");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
